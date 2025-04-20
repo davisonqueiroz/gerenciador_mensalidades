b4266cf4c9c415f8b01a1193ece130ecb799a687
Davison Queiroz <davisonvinicius@outlook.com.br>
2025-04-19 22:40:46 -0300

    passagem de validações

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,8 @@
 class Student < ApplicationRecord
+  validates [:name,:cpf,:gender,:payment_method], presence: true
+  validates [:name,:cpf],uniqueness: true
+  validates :cpf, numericality: {only_integer: true}
+  enum :gender, M:'M',F:'F'
+  enum :payment_method, boleto:'boleto',cartao:'cartão'  
+  has_many :enrollments
 end
--- app/models/university.rb
+++ app/models/university.rb
@@ -1,2 +1,7 @@
 class University < ApplicationRecord
+  validates :name, presence: true
+  validates [:name,:cnpj], uniqueness: true
+  validates :cnpj, numericality: {only_integer: true}
+  enum :type, escola:'escola',creche:'creche'
+  has_many :enrollments
 end
