17e2f2e74eead9baf6a3c74bfd067ea0ef26709c
Davison Queiroz <davisonvinicius@outlook.com.br>
2025-04-20 18:01:12 -0300

    correções no controller de students

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,5 @@
 class StudentsController < ApplicationController
-  before_action :set_student,only: [:show,:edit,:update,:destroy]
+  before_action :set_student, only: [ :show, :edit, :update, :destroy ]
   def index
     @students = Student.all
   end
@@ -12,23 +12,37 @@ class StudentsController < ApplicationController
 
   def create
     @student = Student.new(student_params)
+    if @student.save
+      redirect_to students_url
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
   end
 
   def update
+    if @student.update(student_params)
+      redirect_to students_url(@student)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
-    @student.destroy
+     if @student.destroy
+       redirect_to students_path
+     else
+      redirect_to @student
+     end
   end
 
 
   private
 
   def student_params
-    params.require(:student).permit(:name,:cpf,:gender,:payment_method,:cellphone,:date_of_birth)
+    params.require(:student).permit(:name, :cpf, :gender, :payment_method, :cellphone, :date_of_birth)
   end
 
   def set_student
