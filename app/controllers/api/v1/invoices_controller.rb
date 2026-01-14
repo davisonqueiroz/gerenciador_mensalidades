ef68672dd9c48a47b0f2bf1a14326c8cc8165a44
davison queiroz <davisonvinicius@outlook.com.br>
2026-01-14 20:34:46 -0300

    refactor: standardize controller structure and improve readability across enrollments, invoices, students, and universities controllers

--- app/controllers/api/v1/invoices_controller.rb
+++ app/controllers/api/v1/invoices_controller.rb
@@ -1,60 +1,64 @@
-class InvoicesController < ApplicationController
-  before_action :set_invoice, only: [ :show, :edit, :update, :destroy ]
-  before_action :set_render, only: [ :show, :edit, :update ]
-  def index
-    @invoices = Api::V1::Invoice.all
-    respond_to do |format|
-      format.html
-      format.json { render json: @invoices }
+module Api
+  module V1    
+    class InvoicesController < ApplicationController
+      before_action :set_invoice, only: [ :show, :edit, :update, :destroy ]
+      before_action :set_render, only: [ :show, :edit, :update ]
+      def index
+        @invoices = Api::V1::Invoice.all
+        respond_to do |format|
+          format.html
+          format.json { render json: @invoices }
+        end
+      end
+      def show
+      end
+
+      def new
+        @invoice = Api::V1::Invoice.new
+      end
+
+      def create
+        @invoice = Api::V1::Invoice.new(invoice_params)
+        if @invoice.save
+          redirect_to invoices_url
+        else
+          render :new, status: :unprocessable_entity
+        end
+      end
+
+      def edit
+      end
+
+      def update
+        if @invoice.update(invoice_params)
+          redirect_to invoices_url(@invoice)
+        else
+          render :new, status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        if @invoice.destroy
+          redirect_to invoices_path
+        else
+          redirect_to @invoice
+        end
+      end
+
+
+      private
+
+      def invoice_params
+        params.require(:invoice).permit(:invoice_value, :due_date, :enrollment_id, :status)
+      end
+
+      def set_invoice
+        @invoice = Api::V1::Invoice.find(params[:id])
+      end
+
+      def set_render
+        render json: @invoice if request.format.json?
+      end
     end
-  end
-  def show
-  end
-
-  def new
-    @invoice = Api::V1::Invoice.new
-  end
-
-  def create
-    @invoice = Api::V1::Invoice.new(invoice_params)
-    if @invoice.save
-      redirect_to invoices_url
-    else
-      render :new, status: :unprocessable_entity
-    end
-  end
-
-  def edit
-  end
-
-  def update
-    if @invoice.update(invoice_params)
-      redirect_to invoices_url(@invoice)
-    else
-      render :new, status: :unprocessable_entity
-    end
-  end
-
-  def destroy
-     if @invoice.destroy
-       redirect_to invoices_path
-     else
-      redirect_to @invoice
-     end
-  end
-
-
-  private
-
-  def invoice_params
-    params.require(:invoice).permit(:invoice_value, :due_date, :enrollment_id, :status)
-  end
-
-  def set_invoice
-    @invoice = Api::V1::Invoice.find(params[:id])
-  end
-
-  def set_render
-    render json: @invoice if request.format.json?
   end
 end
--- app/controllers/api/v1/universities_controller.rb
+++ app/controllers/api/v1/universities_controller.rb
@@ -1,60 +1,64 @@
-class UniversitiesController < ApplicationController
-  before_action :set_university, only: [ :show, :edit, :update, :destroy ]
-  before_action :set_render, only: [ :show, :edit, :update ]
-  def index
-    @universities = Api::V1::University.all
-    respond_to do |format|
-      format.html
-      format.json { render json: @universities }
+module Api
+  module V1    
+    class UniversitiesController < ApplicationController
+      before_action :set_university, only: [ :show, :edit, :update, :destroy ]
+      before_action :set_render, only: [ :show, :edit, :update ]
+      def index
+        @universities = Api::V1::University.all
+        respond_to do |format|
+          format.html
+          format.json { render json: @universities }
+        end
+      end
+      def show
+      end
+
+      def new
+        @university = Api::V1::University.new
+      end
+
+      def create
+        @university = Api::V1::University.new(university_params)
+        if @university.save
+          redirect_to universities_url
+        else
+          render :new, status: :unprocessable_entity
+        end
+      end
+
+      def edit
+      end
+
+      def update
+        if @university.update(university_params)
+          redirect_to universities_url(@university)
+        else
+          render :new, status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        if @university.destroy
+          redirect_to universities_path
+        else
+          redirect_to @university
+        end
+      end
+
+
+      private
+
+      def university_params
+        params.require(:university).permit(:name, :cnpj, :university_type)
+      end
+
+      def set_university
+        @university = Api::V1::University.find(params[:id])
+      end
+
+      def set_render
+        render json: @university if request.format.json?
+      end
     end
-  end
-  def show
-  end
-
-  def new
-    @university = Api::V1::University.new
-  end
-
-  def create
-    @university = Api::V1::University.new(university_params)
-    if @university.save
-      redirect_to universities_url
-    else
-      render :new, status: :unprocessable_entity
-    end
-  end
-
-  def edit
-  end
-
-  def update
-    if @university.update(university_params)
-      redirect_to universities_url(@university)
-    else
-      render :new, status: :unprocessable_entity
-    end
-  end
-
-  def destroy
-     if @university.destroy
-       redirect_to universities_path
-     else
-      redirect_to @university
-     end
-  end
-
-
-  private
-
-  def university_params
-    params.require(:university).permit(:name, :cnpj, :university_type)
-  end
-
-  def set_university
-    @university = Api::V1::University.find(params[:id])
-  end
-
-  def set_render
-    render json: @university if request.format.json?
   end
 end
